8ca765d4b1fcd61f6eb0ead4db23b85d2a6eabea
PaulZi <pavel.zimakoff@gmail.com>
2020-05-07 11:26:18 +0300

    jpeg support

--- bin/cli.js
+++ bin/cli.js
@@ -2,7 +2,9 @@
 
 const minimist = require('minimist');
 const fs = require('fs');
+const path = require('path');
 const PNG = require('pngjs').PNG;
+const jpeg = require('jpeg-js');
 const Color = require('../src/color');
 const palette = require('../src/palette');
 
@@ -10,16 +12,25 @@ let args = minimist(process.argv.slice(2));
 let fixed = args.fixed ? args.fixed.split(',') : [];
 fixed = fixed.map(item => Color.hexToRgb(item));
 
-let data = fs.readFileSync(args._[0]);
-let png = PNG.sync.read(data);
-let result = palette(png.data, {
+let filepath = args._[0];
+let data = fs.readFileSync(filepath);
+
+let img;
+let ext = path.extname(filepath).toLowerCase();
+if (ext === '.jpg' || ext === '.jpeg') {
+    img = decodeJpg(data) || decodePng(data);
+} else {
+    img = decodePng(data) || decodeJpg(data);
+}
+
+let result = palette(img.data, {
     fixed:         fixed,
     qtyMax:        args.colors     || 16,
     colorspace:    args.colorspace || 'lab',
     threshold:     args.threshold  || 0.2,
     stopIncQty:    args['stop']    || 3,
     maxIterations: args['steps']   || 100,
-    width:         png.width,
+    width:         img.width,
 });
 let format = args.format || 'text';
 if (format === 'json') {
@@ -28,4 +39,31 @@ if (format === 'json') {
     result.forEach(item => {
         console.log(item.hex);
     });
+}
+
+/**
+ * @param {Buffer} data
+ * @returns {Object}
+ */
+function decodeJpg(data) {
+    let result;
+    try {
+        result = jpeg.decode(data, {
+            formatAsRGBA:   true,
+            colorTransform: true,
+        });
+    } catch (e) {}
+    return result && result.width ? result : null;
+}
+
+/**
+ * @param {Buffer} data
+ * @returns {Object}
+ */
+function decodePng(data) {
+    let result;
+    try {
+        result = PNG.sync.read(data);
+    } catch (e) {}
+    return result && result.width ? result : null;
 }
